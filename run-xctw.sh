54b1ec6dfafec101cb3ca524e6b6e88a501e4994
Jens Harms <au1064@gmail.com>
2020-04-05 17:52:19 +0200

    FIX: create docker home before mounting

--- run-xctw.sh
+++ run-xctw.sh
@@ -3,8 +3,8 @@
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+mkdir -p $HOME/Docker
 docker run -i -v $HOME/Docker:/home/work -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH jeh/xtcw /bin/bash -c \
        "git clone https://github.com/xtforever/xtcw2.git; \
        cd xtcw2 && git pull && make -j4 && cd test_scroll_list \
        && make ; ./cwri -geometry 500x800"
-
