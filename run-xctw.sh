aa369f185b7129a5e80f70fa679c3a440c352bd6
Jens Harms <au1064@gmail.com>
2020-04-05 17:50:35 +0200

    ADD: xtcw container to autostart and compile a xtcw application

---
+++ run-xctw.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#docker build -t jeh/xtcw xtcw
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+docker run -i -v $HOME/Docker:/home/work -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH jeh/xtcw /bin/bash -c \
+       "git clone https://github.com/xtforever/xtcw2.git; \
+       cd xtcw2 && git pull && make -j4 && cd test_scroll_list \
+       && make ; ./cwri -geometry 500x800"
+
